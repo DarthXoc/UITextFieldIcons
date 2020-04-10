c069a3628129348e4b923a6d052e18b0634106b2
Jason Cox <jason@8757nc.com>
2020-04-10 16:08:15 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# UITextField+Icons
+
+## Summary
+This extension provides the ability to easily add action leading and trailing icons to a UITextField. Each icon can be assigned a color and an action that executes when the icon is tapped.
+
+To learn more about this project, please visit it on 
+[GitHub](https://github.com/DarthXoc/UITextField-Icons).
+
+## Installation
+Simply copy _UITextField+Icons.swift_ to your project... that's it! All of the other files included in this project are just for demo purposes.
+
+## Usage 
+You can choose to create a _UITextField_Icons_ control in the same way that you would create a stock _UITextField_ control, either via Storyboards, programmatically or through a mixture of the two.
+
+### Via Storyboards
+1) Select the UITextField that you wish to use
+2) Switch to the Identity Inspector
+3) Update the UITextField's class to 'UITextField_Icons'
+4) Switch to the Attributes Inspector
+5) You can find the configuration options under the _Text Field Icons_ heading
+
+### Via Code
+Create a _UITextField_Icons_ control in the same way that you would create a _UITextField_ control, except that instead of using _UITextField_, use the _UITextField_Icons_ type. Because _UITextField_Icons_ in an extension of _UITextField_, all of _UITextField_'s functions will be inherited.
+
+## Executing Code On Tap
+When a user taps on a leading or trailing icon, two ways are provided to have custom code executed.
+
+### Closures
+The first execution method is via a standard closure. These can be created on `leadingTapAction` and `trailingTapAction` like so:
+```
+textField.leadingTapAction = {
+    print("Closure: You have tapped on the leading icon");
+}
+
+textField.trailingTapAction = {
+    print("Closure: You have tapped on the trailing icon");
+}
+```
+
+### Delegate Methods
+The second way is via a delegate method:
+1) Add the `UITextFieldDelegate` delegate 
+2) Add the `UITextFieldIconsDelegate` delegate
+3) Implment the `UITextFieldIconsLeadingIcon_Tap(sender: UITextField_Icons)` or `UITextFieldIconsTrailingIcon_Tap(sender: UITextField_Icons)` methods
+```
+class ViewController: UIViewController, UITextFieldDelegate, UITextFieldIconsDelegate {
+    // MARK: - Delegates
+    
+    internal func UITextFieldIconsLeadingIcon_Tap(sender: UITextField_Icons) {
+        print("Delegate Method: You have tapped on the leading icon");
+    }
+    
+    internal func UITextFieldIconsTrailingIcon_Tap(sender: UITextField_Icons) {
+        print("Delegate Method: You have tapped on the trailing icon");
+    }
+}
+```
+
+## Reference
+
+| Attribute | Type | Default | Description |
+| --- | --- | --- | --- |
+| iconSpacingLeft | CGFloat | 8 | The spacing that appears to the left of the icon |
+| iconSpacingRight | CGFloat | 8 | The spacing that appears to the right of the icon |
+| leadingColor | UIColor | .placeholderText | The color of the leading icon |
+| leadingIcon | UIImage | nil | The leading icon (as a UIImage) |
+| leadingTapAction | Closure | nil | The action that occurs when the leading icon is tapped |
+| trailingColor | UIColor | .placeholderText | The color of the trailing icon |
+| trailingIcon | UIImage | nil | The trailing icon (as a UIImage) |
+| trailingTapAction | Closure | nil | The action that occurs when the trailing icon is tapped |
+
+## Known Issues
+* Currently you need to implment `UITextFieldDelegate` alongside `UITextFieldIconsDelegate` in your view controller
+* When using the Trailing Icon is configured, the Clear Button will not appear.
---
+++ UITextField+Icons/Extensions/UITextField+Icons.swift
@@ -0,0 +1,203 @@
+//
+//  UITextField+Icons.swift
+//  UITextField+Icons
+//
+//  Created by Jason Cox on 4/2/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import UIKit
+
+protocol UITextFieldIconsDelegate
+{
+    func UITextFieldIconsLeadingIcon_Tap(sender: UITextField_Icons);
+    func UITextFieldIconsTrailingIcon_Tap(sender: UITextField_Icons);
+}
+
+extension UITextFieldIconsDelegate
+{
+    func UITextFieldIconsLeadingIcon_Tap(sender: UITextField_Icons)
+    {
+    }
+    
+    func UITextFieldIconsTrailingIcon_Tap(sender: UITextField_Icons)
+    {
+    }
+}
+
+@IBDesignable class UITextField_Icons: UITextField {
+    // MARK: - Delegates
+    
+    // Setup the delegate
+    private var delegateIcon: UITextFieldIconsDelegate?;
+
+    override var delegate: UITextFieldDelegate?
+    {
+        didSet
+        {
+            delegateIcon = self.delegate as? UITextFieldIconsDelegate;
+        }
+    }
+    
+    // MARK: - Enums
+    
+    private enum Side {
+        case Leading;
+        case Trailing;
+    }
+    
+    // MARK: - Classes
+    
+    private class UIButtonAction: UIButton
+    {
+        var action: (() -> Void)? = nil;
+    }
+    
+    // MARK: - Interface Builder Properties
+    
+    // MARK: Leading icon properties
+    @IBInspectable internal var leadingColor: UIColor? {
+        didSet {
+            // Render the control
+            self.renderControl();
+        }
+    };
+    @IBInspectable internal var leadingIcon: UIImage? {
+        didSet {
+            // Render the control
+            self.renderControl();
+        }
+    };
+    internal var leadingTapAction: (() -> Void)? {
+        didSet {
+            // Render the control
+            self.renderControl();
+        }
+    };
+    
+    // MARK: Trailing icon properties
+    @IBInspectable internal var trailingColor: UIColor? {
+        didSet {
+            // Render the control
+            self.renderControl();
+        }
+    };
+    @IBInspectable internal var trailingIcon: UIImage? {
+        didSet {
+            // Render the control
+            self.renderControl();
+        }
+    };
+    internal var trailingTapAction: (() -> Void)? {
+        didSet {
+            // Render the control
+            self.renderControl();
+        }
+    };
+    
+    // MARK: Icon Spacing
+    @IBInspectable internal var iconSpacingLeft: CGFloat = 8 {
+        didSet {
+            // Render the control
+            self.renderControl();
+        }
+    };
+    @IBInspectable internal var iconSpacingRight: CGFloat = 8 {
+           didSet {
+               // Render the control
+               self.renderControl();
+           }
+       };
+    
+    // MARK: - Control
+    
+    /// Renders the control
+    private func renderControl() {
+        // Check to see if a leading icon was specified
+        if (self.leadingIcon != nil) {
+            // Set the leading icon
+            self.setIcon(side: .Leading, image: self.leadingIcon, color: self.leadingColor, onTapAction: self.leadingTapAction);
+        }
+        
+        // Check to see if a trailing icon was specified
+        if (self.trailingIcon != nil) {
+            // Set the trailing icon
+            self.setIcon(side: .Trailing, image: self.trailingIcon, color: self.trailingColor, onTapAction: self.trailingTapAction);
+        }
+        
+        // Indicate that the view needs laid out
+        self.setNeedsLayout();
+    }
+    
+    // MARK: - General Functions
+    
+    /// Executes a supplied action
+    @objc private func action(sender: UIButtonAction)
+    {
+        // Check to see if an action was supplied
+        if (sender.action != nil)
+        {
+            // Execute the action
+            sender.action!();
+        }
+    }
+    
+    /// Adds an icon to the specified side with the specified color and tap action
+    private func setIcon(side: Side, image: UIImage?, color: UIColor?, onTapAction: (() -> Void)? = nil) {
+        // Setup the button
+        let button: UIButtonAction = UIButtonAction(frame: CGRect(x: CGFloat.zero,
+                                                      y: CGFloat.zero,
+                                                      width: self.iconSpacingLeft + (image?.size.width ?? CGFloat.zero) + self.iconSpacingRight,
+                                                      height: self.frame.size.height));
+        
+        // Check to see if an action was supplied
+        if (onTapAction != nil) {
+            // Add the action
+            button.action = onTapAction;
+        } else {
+            // Add the delegate callback
+            button.action = {
+                // Check to see which icon was tapped
+                if (side == .Leading) {
+                    // Execute the delegate callback for the leading icon
+                    self.delegateIcon?.UITextFieldIconsLeadingIcon_Tap(sender: self);
+                } else {
+                    // Execute the delegate callback for the trailing icon
+                    self.delegateIcon?.UITextFieldIconsTrailingIcon_Tap(sender: self);
+                }
+            }
+        }
+        
+        // Configure the button
+        button.addTarget(self, action: #selector(action(sender:)), for: .touchUpInside);
+        button.adjustsImageWhenHighlighted = onTapAction != nil ? true : false;
+        
+        button.contentEdgeInsets = UIEdgeInsets(top: 0,
+                                                left: self.iconSpacingLeft,
+                                                bottom: 0,
+                                                right: self.iconSpacingRight);
+        button.setImage(image, for: .normal);
+        button.showsTouchWhenHighlighted = false;
+        button.tintColor = color ?? .placeholderText;
+        
+        // Setup the container view
+        let viewContainer: UIView = UIView(frame: CGRect(x: CGFloat.zero,
+                                                         y: CGFloat.zero,
+                                                         width: button.frame.size.width,
+                                                         height: button.frame.size.height));
+        
+        // Add the button to the container view
+        viewContainer.addSubview(button);
+        
+        // Check to see which side the icon should be place on
+        if (side == .Leading) {
+            // Add the icon to the leading side
+            self.leftView = viewContainer;
+            self.leftViewMode = .always;
+        } else if (side == .Trailing) {
+            // Add the icon to the trailing side
+            self.rightView = viewContainer;
+            self.rightViewMode = .always;
+        }
+    }
+}
